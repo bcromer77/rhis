261f6eeb9940e5c9594372fd7ed84887fd65ed55
Bazil <bazil.cromer@ripplexn.com>
2025-09-24 22:23:46 +0100

    Fix: patch next-themes typing for Vercel build

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,11 @@
-'use client'
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  [key: string]: any;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
