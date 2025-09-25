806c4727d24a1e27869f3edb0697af9170538318
Bazil <bazil.cromer@ripplexn.com>
2025-09-25 10:30:11 +0100

    Remove next-themes and lock app to dark mode

--- components/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  // Stripped version: skip next-themes for demo stability
-  return <>{children}</>;
-}
-
